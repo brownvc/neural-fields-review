cbd4a69cbb42c11d6450a98fa84944599a9b834d
Shiqin Yan <digimonyan@gmail.com>
2021-10-15 13:27:03 -0400

    updated statistics page

--- static/js/views/paper_vis_statistics.js
+++ static/js/views/paper_vis_statistics.js
@@ -4,78 +4,37 @@ const start = () => {
     Promise.all([API.getPapers()])
         .then(([papers]) => {
         allPapers = papers;
-            drawTaskLeaderboard(allPapers);
-            drawTechniqueLeaderboard(allPapers);
+        drawKeywordsStatistics(allPapers);
         })
         .catch((e) => console.error(e));
 }
 
-const drawTaskLeaderboard = (allPapers) => {
-    const tasks2freq = {};
-    for (let paper of allPapers) {
-        for (let task of paper.Tasks) {
-            if (! tasks2freq[task]) {
-                tasks2freq[task] = 0;
-            }
-            tasks2freq[task] += 1;
-        }
-    }
-    const tasks2freqEntries = Object.entries(tasks2freq);
-    tasks2freqEntries.sort((a, b) => { if (a[1] == b[1]) { return 0; } else { return a[1] < b[1] ? 1 : -1 } });
-    const tasks = tasks2freqEntries.map((entry) => entry[0]);
-    const freqs = tasks2freqEntries.map((entry) => entry[1]);
-    let data = [
-            {
-                x: tasks,
-                y: freqs,
-                type: 'bar',
-                marker: {
-                    color: 'rgb(190, 217, 114)',
-                    opacity: 0.9,
-                }
-            }
-    ];
-    let layout = {
-        paper_bgcolor: 'rgba(0,0,0,0)',
-        plot_bgcolor: 'rgba(0,0,0,0)',
-        xaxis: { automargin: true },
-        yaxis: { automargin: true },
-    }
-    Plotly.newPlot('task-statistics', data, layout);
-    var myPlot = document.getElementById('task-statistics');
-    myPlot.on('plotly_click', function(data){
-        const labelClicked = data.points[0].x;
-        if (labelClicked) {
-            const urlToOpen = `papers.html?keyword=${labelClicked}`;
-            console.log(urlToOpen);
-            window.open(urlToOpen, '_blank').focus();
-        }
-        });
-}
 
-const drawTechniqueLeaderboard = (allPapers) => {
-    const techniques2freq = {};
+const drawKeywordsStatistics = (allPapers) => {
+    const keywords2freq = {};
     for (let paper of allPapers) {
-        for (let technique of paper.Techniques) {
-            if (! techniques2freq[technique]) {
-                techniques2freq[technique] = 0;
+        for (let keyword of paper.keywords) {
+            //if (keyword.length > 5) keyword = keyword.substring(0, 6) + "...";
+            if (! keywords2freq[keyword]) {
+                keywords2freq[keyword] = 0;
             }
-            techniques2freq[technique] += 1;
+            keywords2freq[keyword] += 1;
         }
     }
-    const techniques2freqEntries = Object.entries(techniques2freq);
-    techniques2freqEntries.sort((a, b) => { if (a[1] == b[1]) { return 0; } else { return a[1] < b[1] ? 1 : -1 } });
-    const techniques = techniques2freqEntries.map((entry) => entry[0]);
-    const freqs = techniques2freqEntries.map((entry) => entry[1]);
+    const keywords2freqEntries = Object.entries(keywords2freq);
+    keywords2freqEntries.sort((a, b) => { if (a[1] == b[1]) { return 0; } else { return a[1] < b[1] ? 1 : -1 } });
+    const keywords = keywords2freqEntries.map((entry) => entry[0]);
+    const freqs = keywords2freqEntries.map((entry) => entry[1]);
     let data = [
             {
-                x: techniques,
-                y: freqs,
+                x: freqs,
+                y: keywords,
                 type: 'bar',
                 marker: {
                     color: 'rgb(190, 217, 114)',
                     opacity: 0.9,
-                }
+                },
+                orientation: 'h'
             }
     ];
     let layout = {
@@ -84,13 +43,12 @@ const drawTechniqueLeaderboard = (allPapers) => {
         xaxis: { automargin: true },
         yaxis: { automargin: true },
     }
-    Plotly.newPlot('technique-statistics', data, layout);
-    var myPlot = document.getElementById('technique-statistics');
+    Plotly.newPlot('keywords-statistics', data, layout);
+    var myPlot = document.getElementById('keywords-statistics');
     myPlot.on('plotly_click', function(data){
-        const labelClicked = data.points[0].x;
+        const labelClicked = data.points[0].y;
         if (labelClicked) {
             const urlToOpen = `papers.html?keyword=${labelClicked}`;
-            console.log(urlToOpen);
             window.open(urlToOpen, '_blank').focus();
         }
         });
