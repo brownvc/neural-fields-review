b44ec9a9501dd30db54ab78c09bb5d266531400c
yxie20 <yiheng_xie@brown.edu>
2021-12-10 14:50:22 -0500

    update routine

--- scripts/spreadsheet_check_error.py
+++ scripts/spreadsheet_check_error.py
@@ -31,7 +31,7 @@ names = {
     "mÃƒÂ¼ller": "müller",
     "SoÅˆa MokrÃ¡": "Soňa Mokrá"
 }
-non_ascii = ["PapierMâché", "Höfer", "Alenyà", "Cortés", "TöRF", "TöRF:", "Fernández", "László"]
+non_ascii = ["PapierMâché", "Höfer", "Alenyà", "Cortés", "TöRF", "TöRF:", "Fernández", "László", "Goldlücke"]
 for k in names:
     non_ascii += names[k].split(" ")
 
